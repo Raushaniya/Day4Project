e828644fc30655e280eaeda34ca754a51ab38f19
Raushaniya <canym83@gmail.com>
2021-05-25 19:23:38 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,11 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
+        //created a new project
+        //enable version control
+        //added .gitignore with .idea folder and target folder, .iml folder
+        //made initial commit by keeping track of all files
+        //shared project on github
 
     }
 }
